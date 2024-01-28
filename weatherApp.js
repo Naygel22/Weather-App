526269047036427142f895be395b66664ef19bf2
Naygel22 <piotrkozlowski14@gmail.com>
2024-01-28 18:00:40 +0100

    app works

--- weatherApp.js
+++ weatherApp.js
@@ -4,11 +4,13 @@ const app = document.querySelector('.app');
 
 const apiKey = "b41a3a5aa59fb23522ce7a9be4c1fc47";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+let lastCity = "";
 
 async function showWeather(city) {
-  const reponse = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  const apiData = await reponse.json();
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const apiData = await response.json();
   app.innerHTML = '';
+  lastCity = city;
   createElements(apiData);
 }
 
@@ -25,6 +27,13 @@ function createElements(data) {
   inputText.classList.add('inputText');
   searchBar.appendChild(inputText);
 
+  //only letters in input
+  inputText.addEventListener('input', () => {
+    let inputCityText = inputText.value.replace(/[^a-zA-Z]/g, ''); 
+    inputText.value = inputCityText.trim(); 
+  });
+  //
+  
   const buttonContainer = document.createElement('div');
   buttonContainer.classList.add('buttonContainer');
   searchBar.appendChild(buttonContainer);
@@ -68,13 +77,44 @@ function createElements(data) {
   app.appendChild(temperature);
   temperature.textContent = `${Math.ceil(data.main.temp - 273.15)}°C`;
 
-
   const cityName = document.createElement('div');
   cityName.classList.add('cityName');
   app.appendChild(cityName);
-  cityName.textContent = inputText.value;
-  
+  cityName.textContent = lastCity;
+
+  const weatherDetails = document.createElement('div');
+  weatherDetails.classList.add('weatherDetails');
+  app.appendChild(weatherDetails);
+
+  const windSection = document.createElement('div');
+  windSection.classList.add('windSection');
+  weatherDetails.appendChild(windSection);
+
+  const windImg = document.createElement('img');
+  windImg.classList.add('windImg');
+  windImg.src = "images/wind.png";
+  windSection.appendChild(windImg);
+
+  const windValue = document.createElement('div');
+  windValue.classList.add('windValue');
+  windValue.textContent = `${data.wind.speed} km/h`;
+  windSection.appendChild(windValue);
+
+  const humiditySection = document.createElement('div');
+  humiditySection.classList.add('humiditySection');
+  weatherDetails.appendChild(humiditySection);
+
+  const humidityImg = document.createElement('img');
+  humidityImg.classList.add('humidityImg');
+  humidityImg.src = "images/humidity.png";
+  humiditySection.appendChild(humidityImg);
+
+  const humidityValue = document.createElement('div');
+  humidityValue.classList.add('humidityValue');
+  humidityValue.textContent = `${data.main.humidity}%`;
+  humiditySection.appendChild(humidityValue);
 }
+
 
 
 
